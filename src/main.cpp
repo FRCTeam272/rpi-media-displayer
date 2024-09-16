b685e619a523886fe85d503d897fcfd5949c7f39
Jake Gadaleta <me@jakegads.dev>
2024-09-16 19:04:54 -0400

    added download file

--- src/main.cpp
+++ src/main.cpp
@@ -7,5 +7,18 @@ int main() {
     printf("Hello, World!\n");
     bool connected = internet_connected();
     printf("Internet connected: %s\n", connected ? "true" : "false");
+
+    if (connected) {
+        const char* url = "https://raw.githubusercontent.com/FRCTeam272/rpi-media-displayer/check_internet/data.csv";
+        const char* destination = "data.csv";
+        if (download_file(url, destination)) {
+            printf("File downloaded successfully to %s\n", destination);
+        } else {
+            printf("Failed to download file from %s\n", url);
+        }
+    } else {
+        printf("No internet connection. Cannot download file.\n");
+    }
+
     return 0;
 }
