c745380c3cbe8d4b7c9d4da8236b7a1a3c802f06
Jake Gads <jake.gads@gmail.com>
2024-09-17 15:47:30 -0400

    changed exe to .exe

--- execute.bash
+++ execute.bash
@@ -2,7 +2,7 @@
 
 # Globbing all .c, .cpp, .h, and .hpp files
 files=$(find . -type f \( -name "*.c" -o -name "*.cpp" -o -name "*.h" -o -name "*.hpp" \))
-
+exe="main.exe"
 # Print the files
 for file in $files; 
 do
@@ -10,7 +10,7 @@ do
 done
 
 # compile the program
-g++ -std=c++11 -o main $files
+g++ -std=c++11 -o $exe $files
 
 # clean up object files
 files=$(find . -type f \( -name "*.o"  -name "*.gch" \))
@@ -23,6 +23,6 @@ done
 
 
 # give the program execution permission
-chmod +x main
+chmod +x $exe
 # run the program
-./main
+./$exe
